7a01f223acf73000f330add9042fb887cf10fc8c
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-09-02 19:47:41 -0700

    Initialize server

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/list", listHandler)
+	err := http.ListenAndServe(":8800", r)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello World")
+}
